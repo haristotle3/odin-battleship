441c3ce2364017e7f50b2fd604d2b9bf2f963b7f
haristotle <haristotlepatil2003@gmail.com>
2025-08-09 23:39:01 +0530

    Remove gameboard parameter from constructor

--- src/playerClass.js
+++ src/playerClass.js
@@ -1,9 +1,15 @@
 import { Harbour } from "./shipClass.js";
-
+import Gameboard from "./gameboardClass.js";
 export default class Player {
-  constructor(name, gameboard) {
+  constructor(name) {
     this.name = name;
-    this.gameboard = gameboard;
+    this.gameboard = new Gameboard();
     this.ships = new Harbour();
+  }
+}
+
+export class Computer extends Player {
+  constructor() {
+    super("Computer");
   }
 }
