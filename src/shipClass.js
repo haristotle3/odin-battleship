d3e3a0fe9b26129fef07fc8d28137a9f33dc1710
haristotle <haristotlepatil2003@gmail.com>
2025-08-09 12:07:36 +0530

    Implement class Harbour

--- src/shipClass.js
+++ src/shipClass.js
@@ -7,12 +7,21 @@ export default class Ship {
 
   hit() {
     if (this.isSunk()) throw new Error(`${this.name} already sunk`);
-    
+
     this.timesHit++;
     return 0;
   }
 
   isSunk() {
     return this.timesHit === this.length;
+  }
+}
+export class Harbour {
+  constructor() {
+    this.carrier = new Ship(5, "Carrier");
+    this.battleship = new Ship(4, "Battleship");
+    this.cruiser = new Ship(3, "Cruiser");
+    this.Submarine = new Ship(3, "Submarine");
+    this.destroyer = new Ship(2, "Destroyer");
   }
 }
