7cf84d389d22b8ebfb0be677fe7336e0addc497c
haristotle <haristotlepatil2003@gmail.com>
2025-08-13 20:07:27 +0530

    Add isGameAgainstComputer() method to class GameController

--- src/GameController.js
+++ src/GameController.js
@@ -4,8 +4,11 @@ import Gameboard from "./GameboardClass.js";
 export default class GameController {
   constructor(playerOneName, playerTwoName, versusComputer) {
     this.player1 = new Player(playerOneName, new Gameboard());
-    this.player2 = versusComputer ? new Computer() : new Player(playerTwoName, new Gameboard());
+    this.player2 = versusComputer
+      ? new Computer()
+      : new Player(playerTwoName, new Gameboard());
     this.currentPlayer = this.player1;
+    this.isGameAgainstComputer = () => versusComputer;
   }
 
   initializeGame() {}
