7e2fefde09c8a589da7d69fbdb865660cc1bd182
haristotle <haristotlepatil2003@gmail.com>
2025-08-15 19:39:56 +0530

    Implement class ScreenController ONLY works for human vs human games Later refactor into a superclass from which HumanGameScreenController and ComputerGameScreenController may inherit from

--- src/ScreenController.js
+++ src/ScreenController.js
@@ -1,19 +1,111 @@
 import DOMInitializationUtilities from "./DOMInitializationUtilities";
+import {
+  player1PlayContainer,
+  player2PlayContainer,
+  pPlayerTurn,
+  pAlert,
+} from "./DOMInitializer";
+import { MISSED, HIT, ALL_SHIPS_SUNK } from "./GameboardClass";
+import EventBus from "./EventBus";
 export default class ScreenController {
   constructor(gameController) {
     this.gameController = gameController;
-    this.player1 = this.gameController.player1;
-    this.player2 = this.gameController.player2;
+    const player1 = this.gameController.player1;
+    const player2 = this.gameController.player2;
 
     this.player1BoardDiv = DOMInitializationUtilities.createBoard(
-      this.player1.gameboard,
+      player1.gameboard,
       "board-1"
     );
     this.player2BoardDiv = DOMInitializationUtilities.createBoard(
-      this.player2.gameboard,
+      player2.gameboard,
       "board-2"
     );
     this.player1HarbourDiv = DOMInitializationUtilities.dockShips("player-1");
     this.player2HarbourDiv = DOMInitializationUtilities.dockShips("player-2");
+
+    EventBus.addEventListener(
+      "startGame",
+      this.#boardClickHandlerInitializer.bind(this)
+    );
+    // callback will have event listeners referencing boardDivs
+    // therefore binding to 'this' is necessary
+  }
+
+  #initUI() {
+    pPlayerTurn.textContent = `${this.gameController.player1.name} to play`;
+    player1PlayContainer.classList.toggle("current-player");
+    return;
+  }
+
+  #switchPlayersUI() {
+    if (player1PlayContainer.classList.toggle("current-player"))
+      pPlayerTurn.textContent = `${this.gameController.player1.name} to play`;
+    if (player2PlayContainer.classList.toggle("current-player"))
+      pPlayerTurn.textContent = `${this.gameController.player2.name} to play`;
+    return;
+  }
+
+  #endGame() {
+    pPlayerTurn.textContent = " ";
+    pAlert.textContent = `🎉 ${this.gameController.currentPlayer.name} wins! 🎉`;
+
+    player1PlayContainer.classList.remove("current-player");
+    player2PlayContainer.classList.remove("current-player");
+
+    const gameContainer = document.querySelector(".game-container");
+    gameContainer.classList.add("end-game");
+    player2PlayContainer.classList.add("end-game");
+    player2PlayContainer.classList.remove("end-game");
+
+    const shipDivs = document.querySelectorAll(".ship");
+    shipDivs.forEach((shipDiv) => {
+      shipDiv.classList.add("end-game");
+    });
+    return;
+  }
+
+  #updateUI(clickedBlock, attackResult) {
+    switch (attackResult) {
+      case HIT:
+        pAlert.textContent = "It's a hit!";
+        clickedBlock.classList.add("hit");
+        break;
+      case MISSED:
+        pAlert.textContent = "It's a miss!";
+        clickedBlock.classList.add("miss");
+        this.#switchPlayersUI();
+        break;
+      case ALL_SHIPS_SUNK:
+        clickedBlock.classList.add("hit");
+        this.#endGame();
+        break;
+    }
+  }
+
+  #blockClickHandler(clickedLocation) {
+    if (/board-[12]/.test(clickedLocation.target.id)) return;
+
+    const clickedBlock = clickedLocation.target;
+    const [attackedY, attackedX] = [
+      Number(clickedBlock.dataset.row),
+      Number(clickedBlock.dataset.col),
+    ];
+
+    const attackResult = this.gameController.playTurn(attackedY, attackedX);
+    if (attackResult === null) return;
+    this.#updateUI(clickedLocation.target, attackResult);
+
+    return;
+  }
+
+  #boardClickHandlerInitializer() {
+    this.#initUI();
+    this.player2BoardDiv.addEventListener("click", (e) =>
+      this.#blockClickHandler(e)
+    );
+    this.player1BoardDiv.addEventListener("click", (e) =>
+      this.#blockClickHandler(e)
+    );
   }
 }
