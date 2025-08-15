9b943c2cd2ee12faf2b8bb98552e7d776f8bcc17
haristotle <haristotlepatil2003@gmail.com>
2025-08-16 02:05:39 +0530

    Add entry point index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+import {
+  HumanGameController,
+  ComputerGameController,
+} from "./GameController.js";
+import {
+  ComputerGameScreenController,
+  HumanGameScreenController,
+} from "./ScreenController.js";
+import {
+  DOMComputerGameInitializer,
+  DOMHumanGameInitializer,
+  playComputerButton,
+  playHumanButton,
+} from "./DOMInitializer.js";
+
+playComputerButton.addEventListener("click", () => {
+  const gc = new ComputerGameController("Player");
+  new DOMComputerGameInitializer(gc);
+  new ComputerGameScreenController(gc);
+});
+
+playHumanButton.addEventListener("click", () => {
+  const gc = new HumanGameController("Player 1", "Player 2");
+  new DOMHumanGameInitializer(gc);
+  new HumanGameScreenController(gc);
+});
