22ac2804a855390734e27854446bc8c5096af66a
haristotle <haristotlepatil2003@gmail.com>
2025-08-16 03:13:26 +0530

    Improve computer player strength Tries to hit adjacent squares when a ship is hit

--- src/PlayerClass.js
+++ src/PlayerClass.js
@@ -1,4 +1,4 @@
-import { ALREADY_ATTACKED } from "./GameboardClass";
+import { ALREADY_ATTACKED, HIT, MISSED } from "./GameboardClass";
 export default class Player {
   constructor(name, gameboard) {
     this.name = name;
@@ -13,24 +13,52 @@ export default class Player {
 export class Computer extends Player {
   constructor(gameboard) {
     super("Computer", gameboard);
+    this.queue = [];
+    this.visited = new Set();
   }
 
-  #getRandomCoordinate() {
-    const randomX = Math.floor(Math.random() * 10);
-    const randomY = Math.floor(Math.random() * 10);
+  #pushAdjacentCoordinatesToQueue() {
+    const directions = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
 
-    return [randomY, randomX];
+    const [prevY, prevX] = this.queue[this.queue.length - 1];
+    directions.forEach((coordinate, i) => {
+      const [newY, newX] = coordinate;
+      const [y, x] = [prevY + newY, prevX + newX];
+
+      if (y >= 0 && y < 10 && x >= 0 && x < 10) this.queue.push([y, x]);
+    });
+  }
+
+  #getCoordinate() {
+    let attackY, attackX;
+    if (this.queue.length === 0) {
+      attackY = Math.floor(Math.random() * 10);
+      attackX = Math.floor(Math.random() * 10);
+    } else {
+      [attackY, attackX] = this.queue.shift();
+    }
+    return [attackY, attackX];
   }
 
   attack(opponentGameboard) {
-    let [randomY, randomX] = this.#getRandomCoordinate();
-    let returnValue;
+    let [attackY, attackX] = this.#getCoordinate();
+    let attackResult;
     while (
-      (returnValue = opponentGameboard.receiveAttack(randomY, randomX)) ===
+      (attackResult = opponentGameboard.receiveAttack(attackY, attackX)) ===
       ALREADY_ATTACKED
     )
-      [randomY, randomX] = this.#getRandomCoordinate();
+      [attackY, attackX] = this.#getCoordinate();
 
-    return { attackResult: returnValue, coordinates: [randomY, randomX] };
+    if (attackResult === HIT) {
+      this.queue.push([attackY, attackX]);
+      this.#pushAdjacentCoordinatesToQueue();
+    }
+
+    return { attackResult, coordinates: [attackY, attackX] };
   }
 }
