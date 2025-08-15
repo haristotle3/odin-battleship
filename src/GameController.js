0ef7a3b105ab542ba8693255be5faeb4a0cb581a
haristotle <haristotlepatil2003@gmail.com>
2025-08-15 17:46:57 +0530

    Implement switchTurns and playTurn method in class GameController ONLY working for Human vs Human games. Later implement a superclass that HumanGameController and ComputerGameController will inherit from

--- src/GameController.js
+++ src/GameController.js
@@ -1,5 +1,5 @@
 import Player, { Computer } from "./PlayerClass.js";
-import Gameboard from "./GameboardClass.js";
+import Gameboard, { ALREADY_ATTACKED } from "./GameboardClass.js";
 
 export default class GameController {
   constructor(playerOneName, playerTwoName, versusComputer) {
@@ -8,8 +8,27 @@ export default class GameController {
       ? new Computer()
       : new Player(playerTwoName, new Gameboard());
     this.currentPlayer = this.player1;
-    this.isGameAgainstComputer = () => versusComputer;
+    this.currentOpponent = this.player2;
   }
 
-  initializeGame() {}
+  #switchTurns() {
+    this.currentPlayer =
+      this.currentPlayer === this.player1 ? this.player2 : this.player1;
+    this.currentOpponent =
+      this.currentOpponent === this.player2 ? this.player1 : this.player2;
+    return;
+  }
+
+  playTurn(attackY, attackX) {
+    const attackResult = this.currentPlayer.attack(
+      this.currentOpponent.gameboard,
+      attackY,
+      attackX
+    );
+
+    if (attackResult === ALREADY_ATTACKED) return null;
+
+    this.#switchTurns();
+    return attackResult;
+  }
 }
